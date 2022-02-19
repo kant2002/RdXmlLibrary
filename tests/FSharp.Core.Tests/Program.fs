aec8a63c185f09f404bd9d8532bfe4daae593f84
Andrii Kurdiumov <kant2002@gmail.com>
2022-02-19 09:37:55 +0600

    Add more examples for FSharp

--- tests/FSharp.Core.Tests/Program.fs
+++ tests/FSharp.Core.Tests/Program.fs
@@ -1,4 +1,8 @@
 ﻿
 
 // For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+printfn "Hello from %s" "F#"
+printfn "Hello float number %f" 1.0
+printfn "Hello integer number %d" 1
+printfn "Hello float (%f) and integer (%d) numbers " 3.0 2
+printfn "%s %s and float %f" "Hello" "world" 2.0
