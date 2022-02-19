198cdaa9f79aa67291a34b041cc42adc83148399
Andrii Kurdiumov <kant2002@gmail.com>
2022-02-19 22:29:47 +0600

    Add example for DateTime column in EF Core

--- tests/Microsoft.EntityFrameworkCore.Sqlite.Tests/Program.cs
+++ tests/Microsoft.EntityFrameworkCore.Sqlite.Tests/Program.cs
@@ -13,12 +13,28 @@ context.Database.EnsureCreated();
 Console.WriteLine("Creating test data!");
 //context.Table1.Add(new Table1() { Id = 1 });
 context.Table2.Add(new Table2() { Id = 1, Value = 2 });
+context.Table3.Add(new Table3 { Id = 1, Value = "Montenegro" });
+context.Table3.Add(new Table3 { Id = 2, Value = "Albania" });
+context.Table3.Add(new Table3 { Id = 3, Value = "Hungary" });
+context.Table4.Add(new Table4 { Id = 1, Value = new DateTime(2021, 1, 1) });
+context.Table4.Add(new Table4 { Id = 2, Value = new DateTime(1961, 4, 12) });
+context.Table4.Add(new Table4 { Id = 3, Value = new DateTime(1986, 4, 26) });
 context.SaveChanges();
 var query = context.Table2.Where(i => i.Value == 2);
 var list = query.ToList();
 foreach (var item in list)
 {
     Console.WriteLine($"Found id: {item.Id}");
+}
+
+foreach (var item in context.Table3.Where(i => i.Id > 2).ToList())
+{
+    Console.WriteLine($"Found id: {item.Value}");
+}
+
+foreach (var item in context.Table4.Where(i => i.Id > 2).ToList())
+{
+    Console.WriteLine($"Chernobyl: {item.Value}");
 }
 
 public class Table1
@@ -30,10 +46,22 @@ public class Table2
     public int Id { get; set; }
     public int Value { get; set; }
 }
+public class Table3
+{
+    public int Id { get; set; }
+    public string Value { get; set; }
+}
+public class Table4
+{
+    public int Id { get; set; }
+    public DateTime Value { get; set; }
+}
 public class MyDbContext : DbContext
 {
     public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
     
     public DbSet<Table1> Table1 { get; set; } = default!;
     public DbSet<Table2> Table2 { get; set; } = default!;
+    public DbSet<Table3> Table3 { get; set; } = default!;
+    public DbSet<Table4> Table4 { get; set; } = default!;
 }
